1ad2fb0bf5958bd62fdc62b0be6328be390092c8
liviaamota <livia.mota11@aluno.ce.gov.br>
2024-09-16 16:24:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let number = prompt ( "inserir um número ")
+let número = prompt ( "inserir um número ")
+number = parseInt (number)
+número = parseInt (número)
+let soma = number + número
+let subtração = number - número
+let multiplicação = number * número
+let divisão = number / número
+alert(number + "+" + número + "=" + soma + "\n" + number + "-" + número + "=" + subtração + "\n" + number + "*" + número + "=" + multiplicação + "\n" + number + "/" + número + "=" + divisão)
+//cadastramento de recrutas
+let nome = window.prompt("qual seu nome?")
+let sobrenome = window.prompt("qual seu sobrenome?")
+let idade = window.prompt("qual sua data de nascimento")   
+let curso = window.prompt("qual seu curso?")
+//window.alert (" olá, " + "  nome  + sobrenome")
+let nascimento = (2024 - idade)
+window.alert(' olá, ' + ' ' +  nome + ' ' + sobrenome + ' ' + ' você estuda ' + ' ' + curso  + ' ' + 'e tem '+ ' ' + nascimento + ' ' + 'anos.'  +' ' +  'legal!')
